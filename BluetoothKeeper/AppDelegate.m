ecc013820c2dc8a150f8ab5cf529b043fa731828
dongsheng <mdongsheng@gmail.com>
2022-10-10 19:03:34 +0800

    Format code

--- BluetoothKeeper/AppDelegate.m
+++ BluetoothKeeper/AppDelegate.m
@@ -21,14 +21,11 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-
     _soundFile = [[NSBundle mainBundle] pathForResource:@"poop" ofType:@"mp3"];
-
     _file = [NSURL fileURLWithPath:_soundFile];
 
     _player = [[AVAudioPlayer alloc] initWithContentsOfURL:_file error:nil];
     _player.volume = 0.001;
-
     [_player prepareToPlay];
 
     [_player play];
@@ -36,7 +33,6 @@
     [self addStatusItem];
 
     __weak AppDelegate *weakSelf = self;
-
     _timer = [NSTimer timerWithTimeInterval:239 repeats:YES block:^(NSTimer * _Nonnull timer) {
         [weakSelf.player play];
     }];
@@ -45,8 +41,6 @@
 }
 
 - (void)addStatusItem {
-
-
     NSStatusBar *systemStatusBar = [NSStatusBar systemStatusBar];
     NSStatusItem *statusItem = [systemStatusBar statusItemWithLength:NSSquareStatusItemLength];
 
@@ -55,16 +49,13 @@
     self.statusItem.button.toolTip = @"我就是一个无情的发声器";
 
     NSMenu *mainMenu = [[NSMenu alloc] initWithTitle:@"退出"];
-
     [mainMenu addItemWithTitle:@"退出" action:@selector(quit:) keyEquivalent:@"Q"];
 
     self.statusItem.menu = mainMenu;
 
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
-}
+- (void)applicationWillTerminate:(NSNotification *)aNotification {}
 
 #pragma mark - Private Methods
 - (void)quit:(NSStatusItem *)item {
